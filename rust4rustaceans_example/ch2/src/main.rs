9b389a2d5800702438ef61bdb772b8277db84557
Eric Zhu <cupofc0t@gmail.com>
2021-06-21 22:56:38 +0800

    read R4R EA version

---
+++ rust4rustaceans_example/ch2/src/main.rs
@@ -0,0 +1,14 @@
+fn main() {
+    //High Level Model
+    //println!("Hello, world!");
+    let mut x;
+    //assert_eq!(x,42); //error
+    x = 42;
+    let y = &x;
+    //*y = 23; //error
+    x = 43;
+    //assert_eq!(*y,42); //error
+
+    //Low Level Model
+    
+}
