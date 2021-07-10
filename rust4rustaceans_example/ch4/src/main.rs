0ea8b01ece53a48b66258287a9bf143fec63bac6
Eric Zhu <cupofc0t@gmail.com>
2021-07-10 22:45:12 +0800

    read about PhatomData<T>

--- rust4rustaceans_example/ch4/src/main.rs
+++ rust4rustaceans_example/ch4/src/main.rs
@@ -5,6 +5,10 @@ fn main() {
     println!("{}",frobnicate3(x).as_ref());
 
     args_types_choice();
+    
+    test_self_destructer();
+
+    test_doc_hidden();
 }
 
 /// three types of functions
@@ -67,5 +71,90 @@ impl ConsInstance for ConsStruct{
 fn test_where_self_sized(){
     fn cal_without_instance(t:&dyn ConsInstance){
         //t.ss();
+    }
+}
+
+/// use wrapper type to use self-implement destructer function
+struct InnerTypeWithoutDrop{
+    member1:String,
+    member2:i32,
+}
+struct WrapperTypeWithDrop(Option<InnerTypeWithoutDrop>);
+impl Drop for WrapperTypeWithDrop{
+    fn drop(&mut self) {
+        match self.0.take() {
+            Some(x) => {
+                x.destroy_innertype();
+            },
+            None => {},
+        }
+        println!("Successfully release InnerTypeWithoutDrop.");
+    }
+}
+impl WrapperTypeWithDrop{
+    fn get_member1(&self)->String{
+        // must go through the `Option` interface.
+        match &self.0 {
+            Some(x) => {
+                x.member1.clone()
+            },
+            None => {
+                "".to_string()
+            }
+        }
+    }
+}
+impl InnerTypeWithoutDrop{
+    fn destroy_innertype(self){
+        //drop self
+        println!("member1 is {}", &self.member1);
+        println!("member2 is {}", &self.member2);
+        //simulate error in releasing resources
+        assert_eq!(self.member1,"error");
+    }
+}
+fn test_self_destructer(){
+    let x = WrapperTypeWithDrop(Some(InnerTypeWithoutDrop{
+        member1:String::from("error"),
+        member2:1,
+    }));
+    let y = x.get_member1();
+    println!("Clone from wrapper type interface: {}",y);
+}
+
+/// 123
+#[doc(hidden)]
+fn test_doc_hidden(){
+    println!("test_doc_hidden.");
+}
+
+/// use zero-sized types to encode states
+struct Grounded;
+struct Launched;
+struct Rocket<State = Grounded>{
+    state: std::marker::PhantomData<State>,
+}
+impl Default for Rocket<Grounded>{
+    fn default() -> Self {
+        Rocket{
+            state: std::marker::PhantomData,
+        }
+    }
+}
+impl Rocket<Grounded>{
+    pub fn launch(self)->Rocket<Launched>{
+        todo!()
+    }
+}
+impl Rocket<Launched>{
+    pub fn accelerate(&self){}
+    pub fn decelerate(&self){}
+}
+impl<State> Rocket<State>{
+    pub fn color()->i32{
+        todo!()
+    }
+    pub fn weight()->i32{
+        todo!()
     }
 }
