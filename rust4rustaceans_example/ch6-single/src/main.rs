7b4d3809a6905a783418b549511d0a0c711c5e02
Eric Zhu <cupofc0t@gmail.com>
2021-07-15 20:35:29 +0800

    complete ch6-single.

--- rust4rustaceans_example/ch6-single/src/main.rs
+++ rust4rustaceans_example/ch6-single/src/main.rs
@@ -1,3 +1,13 @@
 fn main() {
-    println!("Hello, world!");
+    //println!("Hello, world!");
+
+    // test cfg! macro
+    if cfg!(feature = "derive") {
+        println!("feature derive enabled!");
+    }
+}
+#[cfg(feature = "derive")]
+struct Helper{
+    x:i32,
+    y:i32,
 }
