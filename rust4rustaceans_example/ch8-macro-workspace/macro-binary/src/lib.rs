27975e2fb1021551573095b1f29f3d7d9460379f
Eric Zhu <cupofc0t@gmail.com>
2021-07-23 00:34:55 +0800

    rest 21.07.22

--- rust4rustaceans_example/ch8-macro-workspace/macro-binary/src/lib.rs
+++ rust4rustaceans_example/ch8-macro-workspace/macro-binary/src/lib.rs
@@ -23,3 +23,59 @@ mod mac {
 }
 mm!();// path-based scope
 self::mm!();// path-based scope，这几种调用方式都是一样的。
+
+fn test_inner<T>(init:T,frobnify:bool){
+    let xu8 = 1u8;
+    println!("{}",xu8.result());
+}
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn test_1u8_frobnified(){
+        test_inner(1u8, true);
+    }
+    #[test]
+    fn test_1i128_not_frobnified(){
+        test_inner(1i128, false);
+    }
+}
+macro_rules! test_battery {
+    ($($t:ty as $name:ident),*) => {
+        $(
+            #[cfg(test)]
+            mod $name {
+                use super::*;
+                #[test]
+                fn frobnified(){
+                    test_inner::<$t>(1,true);
+                }
+                #[test]
+                fn unfrobnified(){
+                    test_inner::<$t>(1,true);
+                }
+            }
+        )*
+    };
+}
+test_battery! {
+    u8 as u8_test,
+    i128 as i128_test
+}
+trait AddOne {
+    type InnerType;
+    fn result(&self) -> Self::InnerType;
+}
+macro_rules! addone_for_integer {
+    ($($t:ty),*) => {
+        $(
+            impl AddOne for $t{
+                type InnerType = $t;
+                fn result(&self) -> Self::InnerType{
+                    *self + 1
+                }
+            }
+        )*
+    };
+}
+addone_for_integer![u8,i32];
